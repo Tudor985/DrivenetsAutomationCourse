e20cad61e2fbd60c5d883f65dedd16a546476225
Tudor Segarceanu <tsegarceanu@drivenets.com>
2023-02-06 12:00:04 +0200

    First commit in GIT!

---
+++ hello world.py
@@ -0,0 +1,21 @@
+# this is my first code
+print("hello world\n")
+print("hellow world!!")
+print("hello world", "Iwant", sep="bla")
+print("hello world", end="")
+print("hellow world!!")
+print("sdfdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd",
+      "sdfdsfsdfsdfsdfsdfsdfdsfsdfds", sep="")
+print("sdfdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd" 
+      "sdfdsfsdfsdfsdfsdfsdfdsfsdfds")
+
+
+a="23234"
+b="aasdsaf"
+print(a,b, sep="")
+
+
+
+
+
+
